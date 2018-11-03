060c17c61f0b5c074cd47f5c307e417691e47fa2
Spyros Papaioannou <spyros.papaioannou@camelotglobal.com>
2018-11-03 12:57:30 +0000

    Uses dev & prod config separately

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
-const path = require("path")
-const webpack = require("webpack")
+var path = require("path")
+var webpack = require("webpack")
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
-
-module.exports = {
+const args = (() => {
+  return process.argv.slice(2).reduce((args, arg) => {
+    const [key, value] = arg.replace("--", "").split("=")
+    return { ...args, [key]: value }
+  }, {})
+})()
+const MODES = {
+  DEV: "development",
+  PROD: "production"
+}
+const commonConfig = {
   context: path.join(__dirname, "src"),
   entry: ["./index.js"],
-  output: {
-    path: path.join(__dirname, "www"),
-    filename: "bundle.js",
-    publicPath: "www"
-  },
   module: {
     rules: [
       {
@@ -20,24 +25,62 @@ module.exports = {
         }
       }
     ]
+  }
+}
+const prodConfig = {
+  mode: MODES.PROD,
+  devtool: "inline-source-map",
+  output: {
+    path: path.join(__dirname, "www"),
+    filename: "bundle.js",
+    publicPath: "./"
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify(MODES.PROD)
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: "index.html"
+    })
+  ],
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  },
+  performance: {
+    hints: "warning"
+  }
+}
+const devConfig = {
+  mode: MODES.DEV,
+  devtool: "inline-source-map",
+  output: {
+    path: path.join(__dirname, "www"),
+    filename: "bundle.js",
+    publicPath: "./"
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "index.html"
-    }),
-    new webpack.HotModuleReplacementPlugin()
+    })
   ],
-  resolve: {
-    modules: [path.join(__dirname, "node_modules")],
-    extensions: [".js", ".jsx"]
+  devServer: {
+    contentBase: path.join(__dirname, "www"),
+    hot: true,
+    port: 9000
   }
 }
+const config = (mode => {
+  switch (mode) {
+    case MODES.PROD:
+      return { ...commonConfig, ...prodConfig }
+    case MODES.DEV:
+      return { ...commonConfig, ...devConfig }
+    default:
+      return { ...commonConfig, ...prodConfig }
+  }
+})(args.mode)
 
-module.exports.devServer = {
-  contentBase: path.join(__dirname, "./www"),
-  compress: true,
-  hot: true,
-  port: 9000
-}
-
-module.exports.devtool = "inline-source-map"
+module.exports = config
